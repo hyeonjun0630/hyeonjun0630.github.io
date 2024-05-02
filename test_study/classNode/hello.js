94682d624e2d4eec6970c95ca8a74cf77811e112
Haru0630 <hyeonjun@inha.edu>
2024-05-02 09:48:46 +0900

    노드 서버

--- test_study/classNode/hello.js
+++ test_study/classNode/hello.js
@@ -1,10 +1,15 @@
 const http = require("http");
-const server = http.createServer(function (request, responses) {
-  responses.writeHead(200, { Content_Type: "text/plane" });
-  responses.write("Hello this is our first node.js application");
-  responses.end();
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, { Content_Type: "text/plane" });
+  response.write("Hello this is our first node.js application");
+  response.end();
 });
 
 const port = 8080;
 server.listen(port);
-console.log("Server running ar port=" + port);
+console.log("Server running at port=" + port);
+
+// 터미널에 
+// cd hyeonjun0630.github.io/test_study/classNode
+// node hello.js
+// 치
