25ec179e25259a3dce4e3af3fcd4d0f066e1254b
Haru0630 <hyeonjun@inha.edu>
2024-05-02 10:22:29 +0900

    정리

--- test_study/classNode/hello1.js
+++ test_study/classNode/hello1.js
@@ -9,7 +9,7 @@ const port = 8080;
 server.listen(port);
 console.log("Server running at port=" + port);
 
-// 터미널에 
+// 터미널에
 // cd hyeonjun0630.github.io/test_study/classNode
 // node hello.js
-// 치
+// 치기
